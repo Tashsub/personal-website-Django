ca485d0300ffd7117cd38ac63fe85d53fb02d337
Tashinga Musanhu <44523844+Tashsub@users.noreply.github.com>
2020-09-17 00:07:27 +0200

    projects can now be added to home pahe

--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Job
 # Create your views here.
 def home(request): 
-    return render(request, 'jobs/home.html')
+
+    #get all the job objects from the DB and use them as python code
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs':jobs})
