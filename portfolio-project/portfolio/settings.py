eced9b7e17931a129f30e242e5b915891a396aef
Tashinga <tashinga@Tashingas-MacBook-Air.local>
2020-09-24 21:58:27 +0100

    Testing commit for server

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -138,3 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+try: 
+    from loacal_settings import *
+except ImportError:
+    pass
